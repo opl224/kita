c3bf88a420e0c8bd4d193b86fde2a6fa8d1f3ed1
Opal Aja <opank2441@gmail.com>
2025-07-18 02:25:14 +0000

    bertanya dahulu, apakah aplikasi ini tidak bisa di npm runbuild dan meng

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   
   // This allows generating dynamic routes on-demand when using `output: 'export'`.
   // When a user visits a dynamic route not pre-rendered at build time, Next.js
