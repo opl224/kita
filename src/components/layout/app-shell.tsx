d253c4ed816235fc0a1d91c6d5e268128481477d
Opal Aja <opank2441@gmail.com>
2025-08-03 13:05:04 +0000

    animasi json

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -246,7 +246,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
             autoplay={true}
             className="w-96 h-96"
         />
-        <p className="text-muted-foreground text-center font-bold">Buka di perangkat seluler untuk membuka aplikasi.</p>
+        <p className="text-muted-foreground text-center">Buka di perangkat seluler untuk membuka aplikasi.</p>
       </div>
     );
   }
