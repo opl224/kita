a21b9d70df4337f911230e1eb0c523b2412f65fb
Opal Aja <opank2441@gmail.com>
2025-08-04 10:50:03 +0000

    seprti itu masih merah

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,46 @@ const withPWA = withPWAInit({
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  cacheStartUrl: true,
+  dynamicStartUrl: true,
+  manifest: {
+    name: "Kita's",
+    short_name: "Kita's",
+    description: "A modern audio communication app for everyone.",
+    start_url: "/",
+    scope: "/",
+    display: "standalone",
+    background_color: "#f0f2f5",
+    theme_color: "#f0f2f5",
+    lang: "id",
+    dir: "auto",
+    orientation: "portrait",
+    icons: [
+        {
+            "src": "/icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/icons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+        },
+        {
+            "src": "/icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+        },
+        {
+            "src": "/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ],
+    "prefer_related_applications": false
+  },
 });
 
 const nextConfig: NextConfig = {
