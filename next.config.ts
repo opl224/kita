0944369a5638095300215b5d1e8ded1889f2168e
Opal Aja <opank2441@gmail.com>
2025-08-04 12:48:40 +0000

    hapus code yang masih ada pwa dan clean code agar berjalan lebih baik

--- next.config.ts
+++ next.config.ts
@@ -53,12 +53,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
   
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
